2d241334e4bdc4bf1ff95ffa7c2ece94916fbe8b
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-02-01 22:46:25 +0000

    learn more advanced react patterns

---
+++ src/auth-context.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const AuthContext = React.createContext({
+  user: { username: "jakiechan", tagline: "", bio: "" },
+});
+AuthContext.displayName = "AuthContext";
+const AuthProvider = ({ user, ...props }) => (
+  <AuthContext.Provider value={user} {...props} />
+);
+
+function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+export { AuthProvider, useAuth };
